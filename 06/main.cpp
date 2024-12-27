d667c0c6c9aee14d89e08103bd294e01cdb2cd4b
Gleb Petrovicev <gpetrovichev@purestorage.com>
2024-12-27 10:22:05 +0100

    06.part2

--- 06/main.cpp
+++ 06/main.cpp
@@ -5,7 +5,9 @@
 #include <ranges>
 #include <algorithm>
 #include <cassert>
+#include <map>
 #include <numeric>
+#include <set>
 
 using namespace std;
 
@@ -34,6 +36,7 @@ guard_t get_guard(field_t & field) {
             return false;
         });
         if (it != field[i].end()) {
+            *it = '.';
             return {{i, it - field[i].begin()}, direction};
         }
     }
@@ -53,6 +56,37 @@ char turn_right(char dir) {
         default:
             assert(false);
     }
+}
+
+pair<int, int> operator-(const pair<int, int>& lhs, const pair<int, int>& at) {
+    return {lhs.first - at.first, lhs.second - at.second};
+}
+
+pair<int, int> operator+(const pair<int, int>& lhs, const pair<int, int>& at) {
+    return {lhs.first + at.first, lhs.second + at.second};
+}
+
+bool obstruction_here_will_send_to_obstruction(const field_t & field, guard_t guard) {
+    map<char, pair<int, int>> directions = {
+        {'^', {-1, 0}},
+        {'>', {0, 1}},
+        {'v', {1, 0}},
+        {'<', {0, -1}}
+    };
+
+    auto guard_poisition = guard.first - directions.at(guard.second);
+    char new_dir = turn_right(guard.second);
+
+    while (guard_poisition.first >= 0 && guard_poisition.first < field.size() &&
+           guard_poisition.second >= 0 && guard_poisition.second < field[0].size()) {
+        if (field[guard_poisition.first][guard_poisition.second] == '#') {
+            return true;
+        }
+
+        guard_poisition = guard_poisition + directions.at(new_dir);
+    }
+
+    return false;
 }
 
 guard_t move_guard(field_t & field, guard_t guard) {
@@ -91,20 +125,54 @@ guard_t move_guard(field_t & field, guard_t guard) {
         }
     } while (field[new_pos.first][new_pos.second] == '#');
 
+    if (field[pos.first][pos.second] == dir) {
+        throw logic_error("Loop detected");
+    }
+
     field[pos.first][pos.second] = dir;
 
     return {new_pos, dir};
 }
 
 int main() {
-    field_t field = read_field();
-    guard_t guard = get_guard(field);
+    set<pair<int, int>> loop_creating_obstacles = {};
+
+    std::vector<std::vector<char>> initial_field = read_field();
+    const guard_t initial_guard = get_guard(initial_field);
+
+    field_t field = initial_field;
+    guard_t guard = initial_guard;
     try {
         while (true) {
             guard = move_guard(field, guard);
+
+            if (loop_creating_obstacles.contains(guard.first)) {
+                continue;
+            }
+
+            if (initial_field[guard.first.first][guard.first.second] != '.') {
+                continue;
+            }
+
+            field_t obstacle_field = initial_field;
+            obstacle_field[guard.first.first][guard.first.second] = '#';
+            guard_t obstacle_guard = initial_guard;
+            try {
+                while (true) {
+                    obstacle_guard = move_guard(obstacle_field, obstacle_guard);
+                }
+            } catch (const out_of_range &) {
+                // Ignore
+            } catch (const logic_error & e) {
+                loop_creating_obstacles.insert(guard.first);
+            }
         }
-    } catch (out_of_range & e) {
-        cout << "Guard moved out of the field" << endl;
+    } catch (const exception & e) {
+        cout << e.what() << endl;
+    }
+
+    for (auto & obstacle : loop_creating_obstacles) {
+        field[obstacle.first][obstacle.second] = 'O';
     }
 
     for (auto & row : field) {
@@ -120,5 +188,8 @@ int main() {
         });
     });
 
+    assert(!loop_creating_obstacles.contains(initial_guard.first));
+
     cout << "Answer: " << accumulate(res.begin(), res.end(), 0L) << endl;
+    cout << "Obstacles: " << loop_creating_obstacles.size() << endl;
 }
